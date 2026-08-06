b587f77142edb9a9cc845ef95dac375441633cdf
Gerardo-Mantanico <Gerardotax99@gmail.com>
2026-08-05 22:43:42 -0600

    chore: clean workspace and setup root docker compose

--- auth/src/main/java/com/auth/demo/repository/JwtBlocklistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.auth.demo.repository;
-
-import com.auth.demo.model.JwtBlocklist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JwtBlocklistRepository extends JpaRepository<JwtBlocklist, Long> {
-    boolean existsByToken(String token);
-}
